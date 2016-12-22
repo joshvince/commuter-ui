b94954f0b45310680189c1adf8fde7a93e22e597
Josh <joshmvince@gmail.com>
2016-12-22 21:26:25 +0000

    change to browserHistory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory} from 'react-router'
+import { Router, Route, IndexRoute, browserHistory} from 'react-router'
 import App from './App';
 import Home from './home/Home';
 import LineList from './components/LineList/LineList';
@@ -9,7 +9,7 @@ import './index.css';
 
 
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="lines" component={LineList} />
