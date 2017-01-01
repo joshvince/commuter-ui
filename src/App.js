d1f5b6e2f0e58982943f3228c2e7fa33a23cd620
Josh <joshmvince@gmail.com>
2017-01-01 17:53:15 +0000

    new palette

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 injectTapEventPlugin();
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Nav from './components/nav/Nav';
 import './styles/App.css';
+import Theme from './styles/muitheme.js'
+
+const muiTheme = getMuiTheme(Theme)
+
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div id="appContainer">
           <Nav />
           {this.props.children}
---
+++ src/styles/muitheme.js
@@ -0,0 +1,17 @@
+export default {
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: '#294C5D',
+    primary2Color: '#173B4D',
+    primary3Color: '#62808F',
+    accent1Color: '#B8A05F',
+    accent2Color: '#f5f5f5',
+    accent3Color: '#9e9e9e',
+    borderColor: '#90a4ae',
+    pickerHeaderColor: '#294C5D',
+    // textColor: 'rgba(0, 0, 0, 0.87)',
+    // alternateTextColor: '#ffffff',
+    // canvasColor: '#ffffff',
+    // shadowColor: 'rgba(0, 0, 0, 1)'
+  },
+};
