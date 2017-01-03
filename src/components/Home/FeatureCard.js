ef2698708b6c0b6a3a202cdf8be19d0787f64521
Josh <joshmvince@gmail.com>
2017-01-03 17:46:43 +0000

    wip featurecards

---
+++ src/components/Home/FeatureCard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {Card, CardTitle} from 'material-ui/Card';
+
+import './FeatureCard.css'
+
+class FeatureCard extends Component {
+  render() {
+    return (
+      <div className="FeatureCard">
+        Lines
+      </div>
+    )
+  }
+}
+
+// return (
+//   <Card className="FeatureCard">
+//     <CardTitle title="Lines" />
+//   </Card>
+// );
+
+export default FeatureCard;
