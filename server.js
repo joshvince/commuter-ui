c0de5255e42786ab5861f50214cc2a3c70300763
Josh <joshmvince@gmail.com>
2017-01-15 15:06:01 +0000

    change port

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(9000, () => {
-  console.log(`Server is listening on port 9000`)
+app.listen(3000, () => {
+  console.log(`Server is listening on port 3000`)
 });
