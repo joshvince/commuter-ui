ef416622bfe65ed17cc3d0ced9b3b4bbacfe0e89
Josh <joshmvince@gmail.com>
2017-01-15 15:23:18 +0000

    more port updtes

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var port = process.env.PORT || 8081
+
 app.use(express.static('./build'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log(`Server is listening on port 3000`)
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
 });
