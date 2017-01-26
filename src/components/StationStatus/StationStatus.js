844e559edb705973fccb3a7ca5a26bdf6e183e23
Josh <joshmvince@gmail.com>
2017-01-26 08:39:23 +0000

    more wip on station selector

--- src/components/StationStatus/StationStatus.js
+++ src/components/StationStatus/StationStatus.js
@@ -1,26 +1,45 @@
 import React, {Component} from 'react';
-import StationSelector from './StationSelector';
 import Stations from '../../stationData';
 
 class StationStatus extends Component {
   constructor(props) {
     super(props)
+    this.setStation = this.setStation.bind(this);
     this.state = {
-      station: "HINTHERE",
+      station: "Stockwell",
       lines: null,
       directions: null
     }
   }
 
+  setStation(e) {
+    this.setState({
+      station: e.target.value
+    })
+  }
+
   render()  {
     var stationData = Stations.data()
     var stationList = Object.keys(stationData)
+    console.log(`stationdata is: ${JSON.stringify(Object.keys(stationData[this.state.station]))}`)
     return (
       <div>
-        <StationSelector stationList={stationList} />
-        {this.state.station}
+        <div>
+          <input list="station" autoFocus onChange={this.setStation}></input>
+          <datalist id="station">
+            {stationList.map((station, i) => {
+              return <option value={station} key={i}/>
+            })}
+          </datalist>
+        </div>
+        <div>
+          {
+            Object.keys(stationData[this.state.station]).map(line => {
+              return <div key={line}>{line} Line</div>
+            })
+          }
+        </div>
       </div>
-
     )
   }
 }
