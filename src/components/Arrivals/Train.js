a293c3eb03a6b1a1b0e38a57bfe2efbdf629383e
Josh <joshmvince@gmail.com>
2017-02-23 08:59:30 +0000

    add star icons

--- src/components/Arrivals/Train.js
+++ src/components/Arrivals/Train.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import styles from './styles.css'
 
+import quarterStar from '../../img/stars/quarterstar.svg';
+import halfStar from '../../img/stars/halfstar.svg';
+import threeQuarterStar from '../../img/stars/threequarterstar.svg';
+import fullStar from '../../img/stars/fullstar.svg';
+
 class Train extends Component {
   constructor(props){
     super(props)
@@ -9,6 +14,9 @@ class Train extends Component {
   render(){
     return(
       <div className="train-wrapper">
+        <div className="icon-wrapper">
+          <img src={quarterStar}></img>
+        </div>
         <div className="arrival-time-wrapper">
           <h1>{this.props.arrivalTime}</h1>min
 
