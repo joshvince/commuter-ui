f2938e53eade32298a8e5074ae883e6d3f8a4e4a
Josh <joshmvince@gmail.com>
2017-02-28 13:58:25 +0000

    ping the server every 10 mins

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var http = require('http');
 
 var port = process.env.PORT || 8081
 
@@ -9,6 +10,30 @@ app.use(express.static('./build'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
+
+/*
+This pings the heroku dyno hosting the backend app, to stop it from falling
+asleep...
+
+When this gets deployed properly, it can be removed. Yay AWS!
+*/
+setInterval(function(){
+  return http.get(
+    {
+      host: 'choob-service.herokuapp.com',
+      path: '/'
+    }, function(response){
+      var body = ''
+      response.on('data', function(d){
+        body += d
+      });
+      response.on('end', function(){
+        var parsed = JSON.stringify(body)
+        console.log(parsed);
+      })
+    }
+  )
+},600000)
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
